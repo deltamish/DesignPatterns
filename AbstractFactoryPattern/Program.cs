a4e45be28fe45edd4969d389b8a5889b43f320c5
Karthik Hebbar <karthikhebbar600@gmail.com>
2020-12-16 13:27:10 +0530

    Completed AbstractFactory Demo

--- AbstractFactoryPattern/Program.cs
+++ AbstractFactoryPattern/Program.cs
@@ -1,25 +1,52 @@
 ﻿using AbstractFactoryPattern.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 
 namespace AbstractFactoryPattern
 {
     //goal is to create and return a converter calss depeding on the Tool ia mexporitng to Unity,Unreal,Blender
     class Program
     {
+        static string CheckPassFail(bool param)
+        {
+            if (param)
+            {
+                return "Pass";
+            }
+            else
+            {
+                return "Fail";
+            }
+        }
         static void Main(string[] args)
         {
+          
+            HashSet<Type> types = new HashSet<Type>(); // lets store all Types for this particualr factory type
             Console.WriteLine("Write type of Converter you need Unity,Unreal,Blender");
             string converter = Console.ReadLine();
+           
+            converter= converter.ToLowerInvariant();
+            var firstLetter = converter[0].ToString().ToUpperInvariant();
+            converter = converter.Substring(1);
+            converter = converter.Insert(0, firstLetter);
             IConverterFactory converterFactory= null;
             try
             {
-                var type = from cl in Assembly.GetExecutingAssembly().GetTypes()
-                                   where cl.IsClass && cl.Namespace == "AbstractFactoryPattern"
+                var allclasses = from cl in Assembly.GetExecutingAssembly().GetTypes()
+                                 where cl.IsClass && cl.Namespace.Contains("AbstractFactoryPattern")
+                                 select cl;
+                var otherClassTypes = from cl in allclasses
+                                      where cl.FullName.ToUpperInvariant().Contains(converter.ToUpperInvariant())
+                                      select cl;
+
+                var factorytype = from cl in allclasses
                                    where cl.FullName.Equals($"AbstractFactoryPattern.Factories.{converter}ConverterFactory")
                                    select cl;
-                converterFactory =  (IConverterFactory)Activator.CreateInstance(type.First());//Unity Container . get
+                types = new HashSet<Type>( otherClassTypes);
+                converterFactory =  (IConverterFactory)Activator.CreateInstance(factorytype.First());//Unity Container . get
             }
             catch (Exception e)
             {
@@ -27,11 +54,15 @@ namespace AbstractFactoryPattern
             }
 
             if (converterFactory == null) {
-                Console.WriteLine($"{converter} not defined");
+                Console.WriteLine($"{converter} Factory not defined");
             }
             else
             {
-                Console.WriteLine($"{}");
+                Console.WriteLine($"Constructing {converter}Factory");
+                var axisconverter = converterFactory.CreateAxisConverter();
+                var nodeConverter = converterFactory.CreateNodesConverter();
+                Console.WriteLine($"AxisConverter check: {CheckPassFail(types.Contains(axisconverter.GetType()))}");
+                Console.WriteLine($"NodeConverter check: {CheckPassFail(types.Contains(nodeConverter.GetType()))}");
             }
 
             Console.Read();
